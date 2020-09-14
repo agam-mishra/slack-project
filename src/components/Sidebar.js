4bb4552f0d505c2cbfc907148b858120ba6818f7
agam-mishra <realagam.1997@gmail.com>
2020-09-15 00:52:51 +0530

    Add HTML and Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+	render() {
+		return (
+			<div id="sidebar">
+				<div className="user-profile">
+					<div>
+						<img />
+					</div>
+					<div>Ronald</div>
+				</div>
+				<div className="user-channels" />
+			</div>
+		);
+	}
+}
