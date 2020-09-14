6ce12485fcb42bff2096870215fcf80e099883e4
agam-mishra <realagam.1997@gmail.com>
2020-09-15 00:17:45 +0530

    add routing

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SignIn from './SignIn';
+
+function Home() {
+	return <div>Home</div>;
+}
+
+function Some() {
+	return <div>Some</div>;
+}
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path="/" component={SignIn} />
+					<Route exact path="/home" component={Home} />
+					<Route exact path="/some" component={Some} />
+				</Switch>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+	render() {
+		return (
+			<div className="signin-form">
+				<h1>SignIn/ SignUp</h1>
+				<button>Sign IN with Google/ Sign UP with Google</button>
+			</div>
+		);
+	}
+}
+
+export default SignIn;
